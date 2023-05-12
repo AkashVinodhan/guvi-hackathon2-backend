53e0e1165f0bdea5352dc4acaa5a332e5db2cbfa
AkashVinodhan <akashvinodhan@gmail.com>
2023-05-12 09:47:01 +0530

    initail commit

---
+++ Models/productModels.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  duration: {
+    type: String,
+    required: true,
+  },
+  picture: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
---
+++ index.js
@@ -0,0 +1,69 @@
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const Razorpay = require("razorpay");
+
+const mongoose = require("mongoose");
+const Product = require("./Models/productModels");
+const Message = require("./Models/messageModel");
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+// connect to DB
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+  } catch (error) {
+    console.log(error);
+  }
+};
+connectDB();
+
+//listen on port only when DB is connected
+mongoose.connection.once("open", () => {
+  console.log("Connected to DB");
+  app.listen(process.env.PORT, (req, res) => {
+    console.log("Server running in port ", process.env.PORT);
+  });
+});
+
+//api endpoints
+
+app.get("/products", async (req, res) => {
+  const products = await Product.find({});
+  res.send(products);
+});
+
+app.post("/messages", async (req, res) => {
+  const { name, email, description } = req.body;
+  const message = await Message.create({ name, email, description });
+  console.log(message);
+  res.send(message);
+});
+
+const instance = new Razorpay({
+  key_id: process.env.RAZOR_KEY,
+  key_secret: process.env.RAZOR_KEY_SECRET,
+});
+
+app.post("/order", async (req, res) => {
+  try {
+    const { order_id, price, order_details } = req.body;
+
+    const options = {
+      amount: Number(price * 100),
+      currency: "INR",
+    };
+    const order = await instance.orders.create(options);
+    res.send({ success: true, key: process.env.RAZOR_KEY, data: order });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/payment", async (req, res) => {
+  res.send({ success: true });
+});
